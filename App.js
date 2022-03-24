f569ac34c39a1823cc07834b86c01f4582a1f75b
M_Hamid_Zahid <85985817+HamidZahid@users.noreply.github.com>
2022-03-25 00:02:40 +0500

    initial Commit

---
+++ App.js
@@ -0,0 +1,6 @@
+window.addEventListener("scroll", function () {
+  document.querySelector(".first1").style.translateZ = "180px";
+  document.querySelector(".first1").style.rotate = "5deg";
+  document.querySelector(".first1").style.ease = "bounce";
+});
+console.log("hello i am here");
